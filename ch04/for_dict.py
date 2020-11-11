213edba5609b3aeea38a82163b1e5660dc999c9f
sarte3 <sarte@outlook.kr>
2020-11-11 13:50:49 +0900

    11/11

---
+++ ch04/for_dict.py
@@ -0,0 +1,9 @@
+dictionary = {
+    'name': '70 건조 망고',
+    'type': '당절임',
+    'Ingredient': ['망고', '설탕', '메타중아황산나트륨', '치자황색소'],
+    'origin': '필리핀'
+}
+
+for key in dictionary:
+    print(key, ":", dictionary[key])
